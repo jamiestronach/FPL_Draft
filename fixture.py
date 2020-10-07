808ba1e628dcbcea192d693525a84494e27f772f
Jamie Stronach <jastronach@hotmail.com>
2020-10-07 16:07:23 +0100

    Created fixtures class

--- fixture.py
+++ fixture.py
@@ -1,6 +1,8 @@
 from fixture_gen import Fixture_Generator
+from pull_data import Pull_Data
 
 fixtures = Fixture_Generator(38606).get_fixtures()
+data = Pull_Data(38606)
 
 class Fixture():
 
@@ -9,10 +11,29 @@ class Fixture():
         self.team = team
         self.gameweek = gameweek
         self.opposition = fixture_set[team][gameweek]
+        self.team_score = data.get_scores()[self.team][self.gameweek]
+        self.opposition_score = data.get_scores()[self.opposition][self.gameweek]
+        self.outcome_points = self.match_outcome()
 
+    def match_outcome(self):
 
+        if self.team_score > self.opposition_score:
+            return 3
+        elif self.team_score == self.opposition_score:
+            return 1
+        else:
+            return 0
 
-
+    
 
 print(fixtures)
-print(Fixture(150578, 2, fixtures).opposition)
+print(data.get_scores())
+print(Fixture(150578, 0, fixtures).outcome_points)
+        
+
+
+    
+
+
+
+# print(data.get_scores()[150578][0])
