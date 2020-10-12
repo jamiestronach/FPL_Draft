35bf1400551baf2a2bcef086541c697c2486d2b1
Jamie Stronach <jastronach@hotmail.com>
2020-10-12 13:51:11 +0100

    League working, but currently taking 13.14 seconds to complete. Going to play around with the code in an attempt to speed this process up.

--- league.py
+++ league.py
@@ -1,19 +1,21 @@
 from fixture_gen import Fixture_Generator
 import pandas as pd
 from player import Player
+from fixture import Fixture
+from datetime import datetime as dt
 
 
 class League():
 
-    columns = ["Player_Name", "Team_Name", "Wins", "Draws", "Losses", "GD", "Points"]
+    columns = ["Player_ID", "Player_Name", "Team_Name", "Wins", "Draws", "Losses", "GD", "Points"]
 
-    def __init__(self, fixture_round):
+    def __init__(self, fixture_round = None):
         self.league_table = ""
         self.fixture_round = ""
         self.order = ""
 
     def create_player_row(self, player_id):
-        return [Player(player_id).name, Player(player_id).team_name, 0, 0, 0, 0, 0]
+        return [player_id, Player(player_id).name, Player(player_id).team_name, 0, 0, 0, 0, 0]
 
     def create_league_start(self, player_ids):
         league_lst = []
@@ -21,14 +23,51 @@ class League():
         for player in player_ids:
             league_lst.append(self.create_player_row(player))
 
-        return pd.DataFrame(data = league_lst, columns = self.columns)
-    
-    def add_result(self):
+        df = pd.DataFrame(data = league_lst, columns = self.columns)
+
+        return df.set_index("Player_ID")
+
+    def add_result(self, team, gameweek, fixture_set, league):
+        result = Fixture(team, gameweek, fixture_set)
+        
+        league.at[team, "GD"] = league["GD"][team] + result.team_score
+        
+        if result.outcome_points == 3:
+            league.at[team, "Wins"] = league["Wins"][team] + 1
+            league.at[team, "Points"] = league["Points"][team] + 3
+
+        elif result.outcome_points == 1:
+            league.at[team, "Draws"] = league["Draws"][team] + 1
+            league.at[team, "Points"] = league["Points"][team] + 1
+        
+        elif result.outcome_points == 0:
+            league.at[team, "Losses"] = league["Losses"][team] + 1
+
+        return league
+
+    def add_results_player(self):
+
         pass
+
+
+
+    
+    
 
     def loop_results(self):
         pass
 
 
+
+start = dt.now()
 # print(list(Fixture_Generator(38606).get_fixtures().keys()))
-print(League("Hello").create_league_start(list(Fixture_Generator(38606).get_fixtures().keys())))
+fixtures = Fixture_Generator(38606).get_fixtures()
+
+league = League().create_league_start(list(fixtures.keys()))
+
+# # print(league)
+print(Fixture(150578, 1, fixtures).outcome_points)
+print(League().add_result(150578, 1, fixtures, league))
+
+finish = dt.now()
+print(finish - start)
---
+++ main.py
@@ -0,0 +1,28 @@
+from pull_data import Pull_Data 
+
+
+def main(self, league_code):
+    pass
+
+
+
+
+
+league_code = 38606
+
+
+
+league_data = Pull_Data(league_code)
+
+
+
+
+
+
+league_scores = league_data.get_scores()
+print(league_scores)
+
+
+
+
+
